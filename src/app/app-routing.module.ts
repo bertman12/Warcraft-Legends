b9b577d2286b0ff1a4f25e047a3dd2e3901a64da
Gabe Ac <gabriel.ma.2288@gmail.com>
2021-06-29 14:47:33 -0700

    Crated Game Review Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GameReviewComponent } from './game-review/game-review.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'game-review', component: GameReviewComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/game-review/game-review.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-review',
+  templateUrl: './game-review.component.html',
+  styleUrls: ['./game-review.component.css']
+})
+export class GameReviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

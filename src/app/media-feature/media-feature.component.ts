b6bd489d291defdd04582013430037f69a6223c1
bertman12 <eddydaniel94@gmail.com>
2021-07-02 16:33:55 -0700

    adding box shadows

--- src/app/media-feature/media-feature.component.ts
+++ src/app/media-feature/media-feature.component.ts
@@ -1,16 +1,30 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { PlayVideoDirective } from '../custom-directives/play-video.directive';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Renderer2 } from '@angular/core';
+
 @Component({
   selector: 'app-media-feature',
   templateUrl: './media-feature.component.html',
   styleUrls: ['./media-feature.component.css']
 })
-export class MediaFeatureComponent implements OnInit{
+export class MediaFeatureComponent implements OnInit, AfterViewInit{
 
-  constructor() {}
+  constructor(private renderer: Renderer2) {}
+
+  @ViewChild('localVideo') localVideo!: ElementRef<HTMLVideoElement>;
+  videoWidth:number = 0;
 
   ngOnInit(): void {}
 
+  ngAfterViewInit(){
+    this.videoWidth = this.localVideo.nativeElement.scrollWidth;
+  }
 
+  onButtonClick(){
+    console.log(this.localVideo);
+  }
+
+  getVideoWidth(){
+    return this.videoWidth = this.localVideo.nativeElement.scrollWidth;
+  }
 
 }

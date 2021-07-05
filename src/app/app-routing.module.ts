bbf703f026209944a0efada38e9d59b925fe83d9
Gabe Ac <gabriel.ma.2288@gmail.com>
2021-07-05 10:15:18 -0400

    Game-Review Component Rough Draft Layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,15 @@ import { LoginComponent } from './login/login.component';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MediaFeatureComponent } from './media-feature/media-feature.component';
+import { GameReviewComponent } from './game-review/game-review.component';
 
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent },
-  {path: '**', redirectTo: '/', pathMatch: 'full'},
+  // {path: '**', redirectTo: '/', pathMatch: 'full'},
   {path:"", component: HomeComponent}, //home-landing-page
   {path:"login", component: AppComponent}, //login
-  //{path:"game-review", component: GameReviewComponent}, //review
+  {path:"game-review", component: GameReviewComponent}, //review
   {path:"media", component: MediaFeatureComponent}, //media-feature-component
 ];
 

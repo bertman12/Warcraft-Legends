ca66b9dc0420ced87ceef18dd60f097be1a7f4e6
bertman12 <eddydaniel94@gmail.com>
2021-07-07 13:39:54 -0700

    readded navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,12 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { MediaFeatureComponent } from './media-feature/media-feature.component';
 import { RegisterComponent } from './register/register.component';
-
+import { GameReviewComponent } from './game-review/game-review.component';
 
 const routes: Routes = [
   {path: "login", component: LoginComponent },
   {path:"", component: HomeComponent}, //home-landing-page
-  //{path:"game-review-list/:id", component: GameReviewComponent}, //review
+  {path:"game-review/:id", component: GameReviewComponent}, //review
   //{path:"game-review-list", component: GameReviewListComponent}, //review
   {path:"media", component: MediaFeatureComponent}, //media-feature-component
   {path:"register", component: RegisterComponent}
--- src/app/custom-directives/play-video.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-// import { PlayVideoDirective } from './play-video.directive';
-
-// describe('PlayVideoDirective', () => {
-//   it('should create an instance', () => {
-//     const directive = new PlayVideoDirective();
-//     expect(directive).toBeTruthy();
-//   });
-// });
